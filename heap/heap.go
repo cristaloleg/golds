a23b6450ce4ba8136008642680b2d9ebd2ebe2b1
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-04-27 19:08:58 +0200

    new(heap): Heap interface (#5)

---
+++ heap/heap.go
@@ -0,0 +1,10 @@
+package heap
+
+// Heap interface that all heal-like collections implement
+type Heap interface {
+	Push(value interface{})
+	Pop() (value interface{}, ok bool)
+	Top() (value interface{}, ok bool)
+	Values() []interface{}
+	Build(values []interface{})
+}
