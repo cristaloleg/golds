c79cde88f132e0c219a85efe7a91e10bc0b0e8e7
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-04-27 18:26:06 +0200

    new(golds): Container interface

--- golds.go
+++ golds.go
@@ -1,3 +1,15 @@
 package golds
 
 const version = "0.0.0"
+
+// Container basic interface for all collections
+type Container interface {
+	// Size returns amount of elements inside a collection
+	Size() int
+
+	// IsEmpty returns true if collection is empty
+	IsEmpty() bool
+
+	// Clear removes all elements from the collection
+	Clear()
+}
