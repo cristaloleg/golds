25c8d022512feee952eecd8d741e7b3d47d9c6ed
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-06 19:14:41 +0300

    new(set): Set utils

--- set/set.go
+++ set/set.go
@@ -7,3 +7,37 @@ type Set interface {
 	Del(value interface{})
 	Values() []interface{}
 }
+
+// Union returns set that contains all elements of a and b
+func Union(a, b Set) Set {
+	res := NewHashSet()
+	for _, value := range a.Values() {
+		res.Put(value)
+	}
+	for _, value := range b.Values() {
+		res.Put(value)
+	}
+	return res
+}
+
+// Intersection returns all elements from a and b
+func Intersection(a, b Set) Set {
+	res := NewHashSet()
+	for _, value := range a.Values() {
+		if b.Has(value) {
+			res.Put(value)
+		}
+	}
+	return res
+}
+
+// Difference returns all elements from a but not b
+func Difference(a, b Set) Set {
+	res := NewHashSet()
+	for _, value := range a.Values() {
+		if !b.Has(value) {
+			res.Put(value)
+		}
+	}
+	return res
+}
