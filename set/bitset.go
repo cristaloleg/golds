2c5ff7dd166d677d04a421ee113cbef9229f0d58
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-08 13:22:50 +0300

    set: new methods for BitSet (#27)

--- set/bitset.go
+++ set/bitset.go
@@ -1,36 +1,118 @@
 package set
 
+// BitSet data structure
 type BitSet struct {
 	data []uint64
 }
 
+// NewBitSet returns a pointer to new BitSet
 func NewBitSet(size int) *BitSet {
 	b := &BitSet{
-		data: make([]uint64, size/8),
+		data: make([]uint64, size/8+1),
 	}
 	return b
 }
 
+// Clone returns a copy of a BitSet
+func (b *BitSet) Clone() *BitSet {
+	t := &BitSet{}
+	t.data = append(t.data, b.data...)
+	return t
+}
+
+// Set sets given bit to true
 func (b *BitSet) Set(i int) {
 	x, y := b.getIndex(i)
 	b.data[x] |= b.getMask(y)
 }
 
+// SetBulk sets given bits to true
+func (b *BitSet) SetBulk(indexes ...int) {
+	for _, idx := range indexes {
+		x, y := b.getIndex(idx)
+		b.data[x] |= b.getMask(y)
+	}
+}
+
+// SetRange sets bits in range [i,j] to true
+func (b *BitSet) SetRange(i, j int) {
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		b.data[x] |= b.getMask(y)
+	}
+}
+
+// Unset sets given bit to false
 func (b *BitSet) Unset(i int) {
 	x, y := b.getIndex(i)
 	b.data[x] &= b.getMaskInv(y)
 }
 
+// UnsetBulk sets given bits to false
+func (b *BitSet) UnsetBulk(indexes ...int) {
+	for _, idx := range indexes {
+		x, y := b.getIndex(idx)
+		b.data[x] &= b.getMaskInv(y)
+	}
+}
+
+// UnsetRange sets bits in range [i,j] to false
+func (b *BitSet) UnsetRange(i, j int) {
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		b.data[x] &= b.getMaskInv(y)
+	}
+}
+
+// Get return true if bit is true, false otherwise
 func (b *BitSet) Get(i int) bool {
 	x, y := b.getIndex(i)
 	return (b.data[x] & b.getMask(y)) != 0
 }
 
+// GetBulk returns bit status for indexes
+func (b *BitSet) GetBulk(indexes ...int) []bool {
+	res := make([]bool, len(indexes))
+	for i, idx := range indexes {
+		x, y := b.getIndex(idx)
+		res[i] = (b.data[x] & b.getMask(y)) != 0
+	}
+	return res
+}
+
+// GetRange returns bits statuses from range [i,j]
+func (b *BitSet) GetRange(i, j int) []bool {
+	res := make([]bool, j-i+1)
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		res[idx-i] = (b.data[x] & b.getMask(y)) != 0
+	}
+	return res
+}
+
+// Toggle flips bit value
 func (b *BitSet) Toggle(i int) {
 	x, y := b.getIndex(i)
 	b.data[x] ^= b.getMask(y)
 }
 
+// ToggleBulk flips bits values
+func (b *BitSet) ToggleBulk(indexes ...int) {
+	for _, idx := range indexes {
+		x, y := b.getIndex(idx)
+		b.data[x] ^= b.getMask(y)
+	}
+}
+
+// ToggleRange flips bits in range [i,j] to false
+func (b *BitSet) ToggleRange(i, j int) {
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		b.data[x] ^= b.getMask(y)
+	}
+}
+
+// Count returns number of true bits
 func (b *BitSet) Count() int {
 	res := 0
 	for _, v := range b.data {
@@ -39,8 +121,80 @@ func (b *BitSet) Count() int {
 	return res
 }
 
+// Any returns true if at least 1 bit is true
+func (b *BitSet) Any() bool {
+	return b.Count() > 0
+}
+
+// AnyBulk returns true if at least 1 bit from indexes is true
+func (b *BitSet) AnyBulk(indexes ...int) bool {
+	for _, idx := range indexes {
+		x, y := b.getIndex(idx)
+		if (b.data[x] & b.getMask(y)) != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// AnyRange returns true if at least 1 bit from range is true
+func (b *BitSet) AnyRange(i, j int) bool {
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		if (b.data[x] & b.getMask(y)) != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// None returns true if no bits is true
+func (b *BitSet) None() bool {
+	return b.Count() == 0
+}
+
+// NoneBulk returns true if no bits from indexes are true
+func (b *BitSet) NoneBulk(indexes ...int) bool {
+	for _, idx := range indexes {
+		x, y := b.getIndex(idx)
+		if (b.data[x] & b.getMask(y)) != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// NoneRange returns true if no bits in range are true
+func (b *BitSet) NoneRange(i, j int) bool {
+	for idx := i; idx <= j; idx++ {
+		x, y := b.getIndex(idx)
+		if (b.data[x] & b.getMask(y)) != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Next XXX
+func (b *BitSet) Next(i int) int {
+	return 0
+}
+
+// Prev XXX
+func (b *BitSet) Prev(i int) int {
+	return 0
+}
+
+// ShiftLeft XXX
+func (b *BitSet) ShiftLeft(i int) {
+}
+
+// ShiftRight XXX
+func (b *BitSet) ShiftRight(i int) {
+}
+
 func (b *BitSet) getIndex(i int) (int, int) {
-	return i / 8, i % 8
+	return i >> 3, i & 7
 }
 
 func (b *BitSet) getMask(i int) uint64 {
--- set/bitset_test.go
+++ set/bitset_test.go
@@ -3,7 +3,7 @@ package set
 import "testing"
 
 func TestNewBitSet(t *testing.T) {
-	s := NewBitSet(10)
+	s := NewBitSet(5)
 	if s == nil {
 		t.Error("cannot instantiate BitSet")
 	}
@@ -32,5 +32,79 @@ func TestNewBitSet(t *testing.T) {
 
 	if s.Get(0) != true {
 		t.Error("must be true")
+	}
+}
+
+func TestAnyAndNone(t *testing.T) {
+	s := NewBitSet(10)
+	if s == nil {
+		t.Error("cannot instantiate BitSet")
+	}
+
+	if s.Count() != 0 {
+		t.Errorf("must be empty, but have %v", s.Count())
+	}
+
+	if s.Any() == true {
+		t.Error("Any must be false")
+	}
+	if s.None() == false {
+		t.Error("None must be true")
+	}
+
+	s.Set(1)
+
+	if s.Any() == false {
+		t.Error("Any must be true")
+	}
+	if s.None() == true {
+		t.Error("None must be false")
+	}
+
+	s.Unset(1)
+
+}
+
+func TestBulk(t *testing.T) {
+	s := NewBitSet(10)
+	if s == nil {
+		t.Error("cannot instantiate BitSet")
+	}
+
+	s.SetBulk(2, 3, 5, 7)
+	if count := s.Count(); count != 4 {
+		t.Errorf("must be 4, but was %v", count)
+	}
+
+	s.UnsetBulk(2, 3, 5, 7)
+	if count := s.Count(); count != 0 {
+		t.Errorf("must be 0, but was %v", count)
+	}
+
+	s.ToggleBulk(2, 3, 5, 7)
+	if count := s.Count(); count != 4 {
+		t.Errorf("must be 4, but was %v", count)
+	}
+}
+
+func TestRange(t *testing.T) {
+	s := NewBitSet(10)
+	if s == nil {
+		t.Error("cannot instantiate BitSet")
+	}
+
+	s.SetRange(2, 7)
+	if count := s.Count(); count != 6 {
+		t.Errorf("must be 6, but was %v", count)
+	}
+
+	s.UnsetRange(2, 7)
+	if count := s.Count(); count != 0 {
+		t.Errorf("must be 0, but was %v", count)
+	}
+
+	s.ToggleRange(2, 7)
+	if count := s.Count(); count != 6 {
+		t.Errorf("must be 6, but was %v", count)
 	}
 }
