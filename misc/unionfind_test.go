f4783608aa4c3377e4854f45a80f985b913c3d34
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-13 13:04:37 +0300

    misc.UnionFind: fix find func, added test file, updated doc (#40)

---
+++ misc/unionfind_test.go
@@ -0,0 +1,41 @@
+package misc
+
+import "testing"
+
+func TestUnionFind(t *testing.T) {
+	uf := NewUnionFind(10)
+	if uf == nil {
+		t.Error("cannot instantiate UnionFind")
+	}
+
+	for i := 0; i < 10; i += 2 {
+		uf.Union(i, i+1)
+	}
+
+	if value := uf.Count(); value != 5 {
+		t.Errorf("expected 5 got %v", value)
+	}
+
+	for i := 1; i < 10; i += 2 {
+		uf.Union(i-1, i)
+	}
+
+	if value := uf.Count(); value != 5 {
+		t.Errorf("expected 5 got %v", value)
+	}
+
+	for i := 2; i < 10; i += 2 {
+		uf.Union(i-1, i)
+	}
+
+	if value := uf.Count(); value != 1 {
+		t.Errorf("expected 1 got %v", value)
+	}
+}
+
+func BenchmarkUnionFind(t *testing.B) {
+	uf := NewUnionFind(1000000)
+	for i := 0; i < 1000000; i += 2 {
+		uf.Union(i, i+1)
+	}
+}
