1396ad7edc9fd071e836d7d65a3d11bc54ea8a90
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-20 17:55:40 +0200

    tree: SegmentTree test fix

--- interval/segmenttree_test.go
+++ interval/segmenttree_test.go
@@ -10,14 +10,12 @@ func TestSegmentTree(t *testing.T) {
 
 	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	s.Build(values)
-	t.Log(s.Values())
 
 	if value := s.QueryRange(0, 4); value != 10 {
 		t.Errorf("want %v, got %v", 10, value)
 	}
 
 	s.Modify(1, 12)
-	t.Log(s.Values())
 
 	if value := s.QueryRange(0, 4); value != 20 {
 		t.Errorf("want %v, got %v", 20, value)
@@ -26,8 +24,6 @@ func TestSegmentTree(t *testing.T) {
 	for i := 0; i < 4; i++ {
 		s.Modify(i, 12)
 	}
-	s.ModifyRange(0, 4, 12)
-	t.Error(s.Values())
 
 	if value := s.QueryRange(0, 4); value != 48 {
 		t.Errorf("want %v, got %v", 48, value)
