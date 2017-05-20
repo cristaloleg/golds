eb2b3db4c44926c6d0768504e08d50d663f2cf02
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-20 18:15:16 +0200

    heap: BinaryHeap tests

--- heap/binaryheap_test.go
+++ heap/binaryheap_test.go
@@ -1,6 +1,7 @@
 package heap
 
 import "testing"
+import "sort"
 
 func TestNewHeap(t *testing.T) {
 	comp := func(a, b interface{}) bool {
@@ -11,10 +12,31 @@ func TestNewHeap(t *testing.T) {
 		t.Error("cannot instantiate BinaryHeap")
 	}
 
+	_, ok1 := h.Top()
+	_, ok2 := h.Pop()
+	if ok1 || ok2 {
+		t.Errorf("expected to be nil")
+	}
+
 	for i := 0; i < 10; i++ {
 		h.Push(i)
 	}
 	h.Push(100)
+
+	if value := h.Size(); value != 11 {
+		t.Errorf("want size 11, got %v", value)
+	}
+
+	values := h.Values()
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].(int) < values[j].(int)
+	})
+
+	for i := 0; i < 10; i++ {
+		if values[i] != i {
+			t.Errorf("want %v, got %v", i, values[i])
+		}
+	}
 
 	for i := 0; i < 10; i++ {
 		value, ok := h.Pop()
