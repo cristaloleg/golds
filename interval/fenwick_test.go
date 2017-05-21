92d9deb7b393d298b9f65801185ca60e52b346a7
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-21 17:07:43 +0200

    interval: FenwickTree tests (#57)

---
+++ interval/fenwick_test.go
@@ -0,0 +1,26 @@
+package interval
+
+import "testing"
+
+func TestFenwickTree(t *testing.T) {
+	size := 10
+	f := NewFenwickTree(size)
+	if f == nil {
+		t.Error("cannot instantiate FenwickTree")
+	}
+
+	for i := 0; i < size; i++ {
+		f.Update(i, size-i)
+	}
+
+	for i := 0; i < size; i++ {
+		if value := f.Get(i); value != size-i {
+			t.Errorf("want %v at %v, got %v", size-i, i, value)
+		}
+	}
+
+	f.Set(3, 10)
+	if value := f.Get(3); value != 10 {
+		t.Errorf("want %v at %v, got %v", 10, 3, value)
+	}
+}
