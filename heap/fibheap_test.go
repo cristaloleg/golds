764fb78a88198c372516616f8c379d16a51c8810
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-22 18:51:51 +0200

    heap: FibHeap test and benchmark

--- heap/fibheap_test.go
+++ heap/fibheap_test.go
@@ -1,6 +1,12 @@
 package heap
 
 import "testing"
+import "github.com/cristaloleg/golds"
+
+var xFibHeap interface{}
+
+var _ Heap = (*FibHeap)(nil)
+var _ golds.Container = (*FibHeap)(nil)
 
 func TestFibHeap(t *testing.T) {
 	comp := func(a, b interface{}) bool {
@@ -40,7 +46,6 @@ func TestFibHeap(t *testing.T) {
 	if !h.IsEmpty() {
 		t.Errorf("expected to be empty")
 	}
-
 }
 
 func TestFibHeapMultiple(t *testing.T) {
@@ -73,4 +78,52 @@ func TestFibHeapMultiple(t *testing.T) {
 	if value, ok := h.Top(); !ok || value != 100 {
 		t.Errorf("expected 100, got %v", value)
 	}
+
+	h.Clear()
+	if h.Size() != 0 {
+		t.Error("expected to be empty")
+	}
+}
+
+func BenchmarkFibHeapPush(t *testing.B) {
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewFibHeap(comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	t.ResetTimer()
+	t.ReportAllocs()
+	for i := 0; i < t.N; i++ {
+		h.Clear()
+		for i := 0; i < 1000; i++ {
+			h.Push(i)
+		}
+	}
+	t.StartTimer()
+}
+
+func BenchmarkFibHeapPushPop(t *testing.B) {
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewFibHeap(comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	t.ResetTimer()
+	t.ReportAllocs()
+	for i := 0; i < t.N; i++ {
+		for i := 0; i < 1000; i++ {
+			h.Push(i)
+		}
+		for i := 0; i < 1000; i++ {
+			xFibHeap, xFibHeap = h.Pop()
+		}
+
+	}
+	t.StartTimer()
 }
