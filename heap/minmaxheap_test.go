dc4f1e0000403501a013b5d0bee087ea87783b9b
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-06-02 20:01:50 +0200

    heap: get rid of old MinMaxHeap

--- heap/minmaxheap_test.go
+++ heap/minmaxheap_test.go
@@ -1,27 +1,43 @@
 package heap
 
-import (
-	"sort"
-	"testing"
-)
+import "testing"
 
-func TestNewMinMaxHeap(t *testing.T) {
+func TestNewMinMaxHeap2(t *testing.T) {
 	comp := func(a, b interface{}) bool {
 		return a.(int) < b.(int)
 	}
-	h := NewMinMaxHeap(comp)
+	h := NewMinMaxHeap2(comp)
 	if h == nil {
-		t.Error("cannot instantiate MinMaxHeap")
+		t.Error("cannot instantiate MinMaxHeap2")
+	}
+
+	top, ok := h.Top()
+	if ok || top != nil {
+		t.Errorf("must be empty")
+	}
+	if value, ok := h.Min(); ok || value != nil || value != top {
+		t.Errorf("must be empty")
+	}
+	if value, ok := h.Max(); ok || value != nil {
+		t.Errorf("must be empty")
 	}
 
 	for i := 0; i < 10; i++ {
 		h.Push(i)
+		if value, ok := h.Max(); !ok || value != i {
+			t.Errorf("want %v, got %v", i, value)
+		}
+
 	}
+	for i := 0; i < 10; i++ {
+		h.Push(-i)
+	}
+
 	h.Push(-90)
 	h.Push(100)
 
-	if value := h.Size(); value != 12 {
-		t.Errorf("want size 12, got %v", value)
+	if value := h.Size(); value != 22 {
+		t.Errorf("want size %v, got %v", 22, value)
 	}
 
 	if value, ok := h.Min(); !ok || value != -90 {
@@ -30,30 +46,37 @@ func TestNewMinMaxHeap(t *testing.T) {
 	if value, ok := h.Max(); !ok || value != 100 {
 		t.Errorf("expected max value 100, but was %v", value)
 	}
-
-	h.PopMin()
-	h.PopMax()
-
-	tmp := h.Values()
-	values := make([]int, len(tmp))
-	for i := 0; i < len(tmp); i++ {
-		values[i] = tmp[i].(int)
+	if value, ok := h.Pop(); !ok || value != -90 {
+		t.Errorf("expected max value 10, but was %v", value)
 	}
-	sort.Sort(sort.IntSlice(values))
-
-	for i := 0; i < 10; i++ {
-		if values[i] != i {
-			t.Errorf("want %v, got %v", i, values[i])
-		}
+	if value, ok := h.PopMax(); !ok || value != 100 {
+		t.Errorf("expected max value 10, but was %v", value)
 	}
 
 	for i := 0; i < 10; i++ {
-		if value, ok := h.PopMin(); !ok || value != i {
-			t.Errorf("incorrect min value, expected %v got %v", i, value)
+		value, ok := h.Pop()
+		if !ok || value != i-9 {
+			t.Errorf("incorrect value, expected %v got %v", i-9, value)
 		}
+	}
 
-		if value, ok := h.PopMax(); !ok || value != 9-i {
-			t.Errorf("incorrect max value, expected %v got %v", i, value)
-		}
+	h.Clear()
+	if !h.IsEmpty() {
+		t.Error("expected to be empty")
+	}
+
+	h.PushMany(10, 20, 30)
+	if value := h.Size(); value != 3 {
+		t.Errorf("want size %v, got %v", 3, value)
+	}
+
+	if value, ok := h.Pop(); !ok || value != 10 {
+		t.Errorf("expected max value 10, but was %v", value)
+	}
+	if value, ok := h.PopMax(); !ok || value != 30 {
+		t.Errorf("expected max value 20, but was %v", value)
+	}
+	if value, ok := h.Pop(); !ok || value != 20 {
+		t.Errorf("expected max value 20, but was %v", value)
 	}
 }
