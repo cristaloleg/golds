314c2016f3a4f4cecdee5f705c7072669d087216
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-09-12 17:48:06 +0200

    misc UnionFind optimiza storage

--- misc/unionfind.go
+++ misc/unionfind.go
@@ -4,7 +4,6 @@ package misc
 type UnionFind struct {
 	count  int
 	parent []int
-	rank   []int
 }
 
 // NewUnionFind returns pointer to UnionFind
@@ -12,10 +11,9 @@ func NewUnionFind(size int) *UnionFind {
 	u := &UnionFind{
 		count:  size,
 		parent: make([]int, size),
-		rank:   make([]int, size),
 	}
 	for i := 0; i < size; i++ {
-		u.parent[i] = i
+		u.parent[i] = -1
 	}
 	return u
 }
@@ -25,19 +23,22 @@ func (u *UnionFind) Count() int {
 	return u.count
 }
 
+// Size returns number of independent sets
+func (u *UnionFind) Size(x int) int {
+	return -u.parent[x]
+}
+
 // Union unites two sets
 func (u *UnionFind) Union(x, y int) {
 	x, y = u.find(x), u.find(y)
 	if x == y {
 		return
 	}
-	if u.rank[x] < u.rank[y] {
-		u.parent[x] = y
-		u.rank[y] += u.rank[x]
-	} else {
-		u.parent[y] = x
-		u.rank[x] += u.rank[y]
+	if u.parent[x] > u.parent[y] {
+		x, y = y, x
 	}
+	u.parent[x] += u.parent[y]
+	u.parent[y] = x
 	u.count--
 }
 
@@ -47,9 +48,8 @@ func (u *UnionFind) IsUnited(x, y int) bool {
 }
 
 func (u *UnionFind) find(x int) int {
-	for u.parent[x] != x {
+	for u.parent[x] >= 0 {
 		x = u.parent[x]
-		u.parent[x] = x
 	}
 	return x
 }
