cb7086698519b89fbe47bdefb241bab211059f09
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-09-15 15:54:17 +0200

    graph: LabeledEdge, structs embedding

--- graph/graph.go
+++ graph/graph.go
@@ -15,9 +15,23 @@ type DirectedEdge interface {
 	Target() Vertex
 }
 
+// LabeledEdge ...
+type LabeledEdge interface {
+	Edge
+	Label() string
+}
+
 // WeightedEdge ...
 type WeightedEdge interface {
 	Edge
+	Weight() float64
+}
+
+// DirectedWeightedEdge ...
+type DirectedWeightedEdge interface {
+	Edge
+	Source() Vertex
+	Target() Vertex
 	Weight() float64
 }
 
@@ -26,11 +40,16 @@ type edge struct {
 }
 
 type directedEdge struct {
-	u, v Vertex
+	Edge
+}
+
+type labeledEdge struct {
+	Edge
+	label string
 }
 
 type weightedEdge struct {
-	u, v   Vertex
+	Edge
 	weight float64
 }
 
@@ -46,30 +65,54 @@ func (e edge) Vertices() (u, v Vertex) {
 
 // NewDirectedEdge ...
 func NewDirectedEdge(u, v Vertex) DirectedEdge {
-	e := directedEdge{u: u, v: v}
+	e := directedEdge{
+		Edge: edge{u: u, v: v},
+	}
 	return e
 }
 
 func (e directedEdge) Vertices() (u, v Vertex) {
-	return e.u, e.v
+	return e.Vertices()
 }
 
 func (e directedEdge) Source() (u Vertex) {
-	return e.u
+	u, _ = e.Vertices()
+	return u
 }
 
 func (e directedEdge) Target() (v Vertex) {
-	return e.v
+	_, v = e.Vertices()
+	return v
+}
+
+// NewLabeledEdge ...
+func NewLabeledEdge(u, v Vertex, s string) LabeledEdge {
+	e := labeledEdge{
+		Edge:  edge{u, v},
+		label: s,
+	}
+	return e
+}
+
+func (e labeledEdge) Vertices() (u, v Vertex) {
+	return e.Vertices()
+}
+
+func (e labeledEdge) Label() string {
+	return e.label
 }
 
 // NewWeightedEdge ...
 func NewWeightedEdge(u, v Vertex, w float64) WeightedEdge {
-	e := weightedEdge{u: u, v: v, weight: w}
+	e := weightedEdge{
+		Edge:   edge{u, v},
+		weight: w,
+	}
 	return e
 }
 
 func (e weightedEdge) Vertices() (u, v Vertex) {
-	return e.u, e.v
+	return e.Vertices()
 }
 
 func (e weightedEdge) Weight() float64 {
