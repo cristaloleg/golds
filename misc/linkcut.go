77e0a3aa7dc9b6e074312d16545a8f990e84ae37
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-04-08 20:09:38 +0200

    misc: link-cut tree

---
+++ misc/linkcut.go
@@ -0,0 +1,215 @@
+package misc
+
+// LinkCutTree ...
+type LinkCutTree struct {
+	root []*linkCutNode
+}
+
+// NewLinkCutTree ...
+func NewLinkCutTree(size int) *LinkCutTree {
+	t := &LinkCutTree{
+		root: make([]*linkCutNode, size),
+	}
+	for i := range t.root {
+		t.root[i].label = i
+		t.root[i].update()
+	}
+	return t
+}
+
+// Link ...
+func (t *LinkCutTree) Link(i, j int) {
+	t.root[i].link(t.root[j])
+}
+
+// Cut ...
+func (t *LinkCutTree) Cut(i int) {
+	t.root[i].cut()
+}
+
+// Root ...
+func (t *LinkCutTree) Root(i int) int {
+	return t.root[i].root().label
+}
+
+// Depth ...
+func (t *LinkCutTree) Depth(i int) int {
+	return t.root[i].depth()
+}
+
+// LCA ...
+func (t *LinkCutTree) LCA(i, j int) int {
+	return t.root[i].lca(t.root[j]).label
+}
+
+type linkCutNode struct {
+	size                            int
+	label                           int
+	parent, pathParent, left, right *linkCutNode
+}
+
+func newNode() *linkCutNode {
+	n := &linkCutNode{
+		size:       0,
+		label:      0,
+		parent:     nil,
+		pathParent: nil,
+		left:       nil,
+		right:      nil,
+	}
+	return n
+}
+
+func (x *linkCutNode) update() {
+	x.size = 1
+	if x.left != nil {
+		x.size += x.left.size
+	}
+	if x.right != nil {
+		x.size += x.right.size
+	}
+}
+
+func (x *linkCutNode) rotateRight() {
+	y := x.parent
+	z := y.parent
+
+	y.left = x.right
+	if y.left != nil {
+		y.left.parent = y
+	}
+
+	x.right = y
+	y.parent = x
+
+	x.parent = z
+	if x.parent != nil {
+		if y == z.left {
+			z.left = x
+		} else {
+			z.right = x
+		}
+	}
+	x.pathParent = y.pathParent
+	y.pathParent = nil
+	y.update()
+}
+
+func (x *linkCutNode) rotateLeft() {
+	y := x.parent
+	z := y.parent
+
+	y.right = x.left
+	if y.right != nil {
+		y.right.parent = y
+	}
+	x.left = y
+	y.parent = x
+
+	x.parent = z
+	if x.parent != nil {
+		if y == z.left {
+			z.left = x
+		} else {
+			z.right = x
+		}
+	}
+	x.pathParent = y.pathParent
+	y.pathParent = nil
+	y.update()
+}
+
+func (x *linkCutNode) splay() {
+	for x.parent != nil {
+		y := x.parent
+		if y.parent == nil {
+			if x == y.left {
+				x.rotateRight()
+			} else {
+				x.rotateLeft()
+			}
+		} else {
+			z := y.parent
+			if y == z.left {
+				if x == y.left {
+					y.rotateRight()
+					x.rotateRight()
+				} else {
+					x.rotateLeft()
+					x.rotateRight()
+				}
+			} else {
+				if x == y.right {
+					y.rotateLeft()
+					x.rotateLeft()
+				} else {
+					x.rotateRight()
+					x.rotateLeft()
+				}
+			}
+		}
+	}
+	x.update()
+}
+
+func (x *linkCutNode) access() *linkCutNode {
+	x.splay()
+	if x.right != nil {
+		x.right.pathParent = x
+		x.right.parent = nil
+		x.right = nil
+		x.update()
+	}
+
+	last := x
+	for x.pathParent != nil {
+		y := x.pathParent
+		last = y
+		y.splay()
+		if y.right != nil {
+			y.right.pathParent = y
+			y.right.parent = nil
+		}
+
+		y.right = x
+		x.parent = y
+		x.pathParent = nil
+		y.update()
+		x.splay()
+	}
+	return last
+}
+
+func (x *linkCutNode) root() *linkCutNode {
+	x.access()
+	for x.left != nil {
+		x = x.left
+	}
+	x.splay()
+	return x
+}
+
+func (x *linkCutNode) cut() {
+	x.access()
+	x.left.parent = nil
+	x.left = nil
+	x.update()
+}
+
+func (x *linkCutNode) link(y *linkCutNode) {
+	x.access()
+	y.access()
+	x.left = y
+	y.parent = x
+	x.update()
+}
+
+func (x *linkCutNode) lca(y *linkCutNode) *linkCutNode {
+	x.access()
+	return y.access()
+}
+
+func (x *linkCutNode) depth() int {
+	x.access()
+	return x.size - 1
+}
