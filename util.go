eab8ffba4ec1f652f4d8dcc4642335537bfeac9c
Oleg Kovalov <oleg.kovalov@allegro.pl>
2019-11-20 23:09:44 +0100

    add utils

---
+++ util.go
@@ -0,0 +1,26 @@
+package golds
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func nextPowerOf2(n int32) int32 {
+	n--
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	n++
+	return n
+}
