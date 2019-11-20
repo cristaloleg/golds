3f05acfcb07e6f784cff579ac309965e274330db
Oleg Kovalov <oleg.kovalov@allegro.pl>
2019-11-20 16:54:56 +0100

    add BinaryHeap

---
+++ binaryheap.go
@@ -0,0 +1,135 @@
+package golds
+
+// BinaryHeap ...
+type BinaryHeap struct {
+	size int
+	data []interface{}
+	comp CmpFunc
+}
+
+// NewBinaryHeap instantiates a new BinaryHeap
+func NewBinaryHeap(comp CmpFunc) *BinaryHeap {
+	h := &BinaryHeap{
+		size: 0,
+		data: make([]interface{}, 0),
+		comp: comp,
+	}
+	return h
+}
+
+// NewBinaryHeapSized instantiates a new BinaryHeap
+func NewBinaryHeapSized(size int, comp CmpFunc) *BinaryHeap {
+	h := &BinaryHeap{
+		size: 0,
+		data: make([]interface{}, size),
+		comp: comp,
+	}
+	return h
+}
+
+// Size return amount of keys in the heap
+func (h *BinaryHeap) Size() int {
+	return h.size
+}
+
+// IsEmpty returns true if heap is empty
+func (h *BinaryHeap) IsEmpty() bool {
+	return h.size == 0
+}
+
+// Clear removes all elements from the heap
+func (h *BinaryHeap) Clear() {
+	h.size = 0
+	h.data = h.data[:0]
+}
+
+// Build pushes all items from values to the heap
+func (h *BinaryHeap) Build(values []interface{}) {
+	size := len(values)
+	if len(h.data) < size {
+		h.data = make([]interface{}, size)
+	}
+	copy(h.data, values)
+	h.size = size
+	for i := h.size/2 - 1; i >= 0; i-- {
+		h.down(i)
+	}
+}
+
+// Push adds element to the heap
+func (h *BinaryHeap) Push(value interface{}) {
+	if len(h.data) >= h.size {
+		h.data = append(h.data, nil)
+	}
+	h.data[h.size] = value
+	h.up(h.size)
+	h.size++
+}
+
+// PushMany adds elements to the heap
+func (h *BinaryHeap) PushMany(values ...interface{}) {
+	for _, v := range values {
+		h.Push(v)
+	}
+}
+
+// Pop removes and returns top element of the heap
+func (h *BinaryHeap) Pop() (value interface{}, ok bool) {
+	if h.size == 0 {
+		return nil, false
+	}
+	h.size--
+	h.swap(0, h.size)
+	h.down(0)
+	value = h.data[h.size]
+	h.data = h.data[:h.size]
+	return value, true
+}
+
+// Top returns top element of the heap
+func (h *BinaryHeap) Top() (value interface{}, ok bool) {
+	if h.size == 0 {
+		return nil, false
+	}
+	return h.data[0], true
+}
+
+// Values returns values presented in heap
+func (h *BinaryHeap) Values() []interface{} {
+	return h.data[:h.size]
+}
+
+// down pushes element down in the heap
+func (h *BinaryHeap) down(i int) {
+	for {
+		j := 2*i + 1
+		if j >= h.size {
+			break
+		}
+		if j2 := j + 1; j2 < h.size && h.comp(h.data[j2], h.data[j]) {
+			j = j2
+		}
+		if h.comp(h.data[i], h.data[j]) {
+			break
+		}
+		h.swap(i, j)
+		i = j
+	}
+}
+
+// up pushes element up in the heap
+func (h *BinaryHeap) up(i int) {
+	for {
+		j := (i - 1) / 2
+		if !h.comp(h.data[i], h.data[j]) {
+			break
+		}
+		h.swap(j, i)
+		i = j
+	}
+}
+
+// swap swaps elements on the given indexes
+func (h *BinaryHeap) swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+}
---
+++ binaryheap_test.go
@@ -0,0 +1,146 @@
+package golds
+
+import (
+	"sort"
+	"testing"
+)
+
+var xBinaryHeap interface{}
+
+func TestBinaryHeap(t *testing.T) {
+
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewBinaryHeap(comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	_, ok1 := h.Top()
+	_, ok2 := h.Pop()
+	if ok1 || ok2 {
+		t.Errorf("expected to be nil")
+	}
+
+	for i := 0; i < 10; i++ {
+		h.Push(i)
+	}
+	h.Push(100)
+
+	if value := h.Size(); value != 11 {
+		t.Errorf("want size 11, got %v", value)
+	}
+
+	tmp := h.Values()
+	values := make([]int, len(tmp))
+	for i := 0; i < len(tmp); i++ {
+		values[i] = tmp[i].(int)
+	}
+	sort.Sort(sort.IntSlice(values))
+
+	for i := 0; i < 10; i++ {
+		if values[i] != i {
+			t.Errorf("want %v, got %v", i, values[i])
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		value, ok := h.Pop()
+		if !ok || value != i {
+			t.Errorf("incorrect value, expected %v got %v", i, value)
+		}
+	}
+
+	if value, ok := h.Top(); !ok || value != 100 {
+		t.Errorf("expected 100, got %v", value)
+	}
+
+	h.PushMany(10, 20, 30)
+	for i := 1; i <= 3; i++ {
+		value, ok := h.Pop()
+		if !ok || value != i*10 {
+			t.Errorf("incorrect value, expected %v got %v", i*10, value)
+		}
+	}
+}
+
+func TestBinaryHeapBuild(t *testing.T) {
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewBinaryHeapSized(10, comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	values := make([]interface{}, 100)
+	for i := 0; i < 100; i++ {
+		if i <= 50 {
+			values[i] = i
+		} else {
+			values[i] = 100 - i
+		}
+	}
+	h.Build(values)
+
+	if value := h.Size(); value != 100 {
+		t.Errorf("expected size %v, got %v", 100, value)
+	}
+
+	h.Pop()
+
+	for i := 1; i < 50; i++ {
+		value1, ok1 := h.Pop()
+		value2, ok2 := h.Pop()
+		if !ok1 || !ok2 || value1 != i || value2 != i {
+			t.Errorf("incorrect values, expected %v, got %v and %v", i, value1, value2)
+		}
+	}
+
+	h.Clear()
+	if !h.IsEmpty() {
+		t.Errorf("should be empty")
+	}
+}
+
+func BenchmarkBinaryHeapPush(t *testing.B) {
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewBinaryHeap(comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	t.ReportAllocs()
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		for i := 0; i < 1000; i++ {
+			h.Push(i)
+		}
+	}
+	t.StartTimer()
+}
+
+func BenchmarkBinaryHeapPushPop(t *testing.B) {
+	comp := func(a, b interface{}) bool {
+		return a.(int) < b.(int)
+	}
+	h := NewBinaryHeap(comp)
+	if h == nil {
+		t.Error("cannot instantiate BinaryHeap")
+	}
+
+	t.ReportAllocs()
+	t.ResetTimer()
+	for i := 0; i < t.N; i++ {
+		for i := 0; i < 1000; i++ {
+			h.Push(i)
+		}
+		for i := 0; i < 1000; i++ {
+			xBinaryHeap, xBinaryHeap = h.Pop()
+		}
+	}
+	t.StartTimer()
+}
