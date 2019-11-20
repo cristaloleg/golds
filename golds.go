d64098168158504a7a7dfa519be4d5cba01b5706
Oleg Kovalov <oleg.kovalov@allegro.pl>
2019-11-20 23:09:29 +0100

    add comparers

--- golds.go
+++ golds.go
@@ -14,3 +14,18 @@ type Container interface {
 
 // CmpFunc returns true if a is greater then b.
 type CmpFunc func(a, b interface{}) bool
+
+// IntCmp to compare two int params.
+func IntCmp(a, b interface{}) bool {
+	return a.(int) < b.(int)
+}
+
+// FloatCmp to compare two float params.
+func FloatCmp(a, b interface{}) bool {
+	return a.(float64) < b.(float64)
+}
+
+// StringCmp to compare two string params.
+func StringCmp(a, b interface{}) bool {
+	return a.(string) < b.(string)
+}
